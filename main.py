f1bce1aa1b85da9a8d5d4bcc4b69d943ea5066da
nvLast86 <n.lastenko@gmail.com>
2023-03-09 09:59:37 +0600

    The code has been added in main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+from googleapiclient.discovery import build
+import json
+
+class Channel():
+    api_key = youtube_api
+
+    def __init__(self, channel_id):
+        self.channel_id = channel_id
+        self.channel_json_info = ''
+        self.get_channel_json_info()
+
+    def get_channel_json_info(self):
+        with build('youtube', 'v3', developerKey=Channel.api_key) as channel_info:
+            channel = channel_info.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+            self.channel_json_info = json.dumps(channel, indent = 2, ensure_ascii=False)
+
+    def print_info(self):
+        print(self.channel_json_info)
+
