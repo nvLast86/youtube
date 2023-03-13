e898995ef7db0425ac2b85190d8bdbd30c3c225c
nvLast86 <n.lastenko@gmail.com>
2023-03-13 15:10:59 +0600

    main.py has been changed

--- main.py
+++ main.py
@@ -3,9 +3,16 @@ from utils.channel import Channel
 def main():
     vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
 
-    print(vdud.print_info())
     print(vdud.title)
     print(vdud.url)
+    print(vdud.channel_view_count)
+
+    vdud.__channel_id = 'New title'
+
+    print(Channel.get_service())
+
+    vdud.to_json('vdud.json')
+
 
 
 
