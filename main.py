7d5d67d4fbf8597bc88b1711ace3b521b4433858
nvLast86 <n.lastenko@gmail.com>
2023-03-13 14:20:26 +0600

    main.py has been changed according to task #2

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ from utils.channel import Channel
 
 def main():
     vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
-    vdud.print_info()
+
+    print(vdud.print_info())
+    print(vdud.title)
+    print(vdud.url)
+
+
 
 
 if __name__ == '__main__':
