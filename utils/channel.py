8051309d14889026957854cdd39c1358ce92dd85
nvLast86 <n.lastenko@gmail.com>
2023-03-13 14:59:01 +0600

    get_json_by_id() has been added

--- utils/channel.py
+++ utils/channel.py
@@ -6,29 +6,21 @@ class Channel:
     api_key = 'AIzaSyC-4j13M0Xk0NnC4w4BVanF64fO5Lv0uUI'
 
     def __init__(self, channel_id):
-        youtube = build('youtube', 'v3', developerKey='AIzaSyC-4j13M0Xk0NnC4w4BVanF64fO5Lv0uUI')
         self.__channel_id = channel_id
-        self.channel_json_info = youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
-        #self.get_channel_json_info()
+        self.channel_json_info = self.get_json_by_id()
         self.title = self.channel_json_info['items'][0]['snippet']['title']
         self.channel_description = self.channel_json_info['items'][0]['snippet']['description']
         self.url = r'https://www.youtube.com/channel/' + self.__channel_id
         self.video_count = self.channel_json_info['items'][0]['statistics']['videoCount']
         self.channel_view_count = self.channel_json_info['items'][0]['statistics']['viewCount']
 
-    def get_channel_json_info(self):
-        """Метод получения информации о канале в формате json"""
-        with build('youtube', 'v3', developerKey=Channel.api_key) as channel_info:
-            channel = channel_info.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
-            print(channel)
-         #   self.channel_json_info = json.dumps(channel, indent=2, ensure_ascii=False)
-         #   print(self.channel_json_info)
-
     def get_json_by_id(self):
-        """ метод создает специальный объект для работы с API youtube"""
-        channel = self.channel_info.channels().list(id=self.__id, part='snippet,statistics').execute()
-        self.channel_json_info = channel
+        """ получаем данные о канале по его ip"""
+        youtube = build('youtube', 'v3', developerKey='AIzaSyC-4j13M0Xk0NnC4w4BVanF64fO5Lv0uUI')
+        return youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+
 
     def print_info(self):
+
         print(self.channel_json_info)
 
