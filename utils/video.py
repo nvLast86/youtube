1031e81c74c1f0454ae133e46ae01ba1a5a4baf2
nvLast86 <n.lastenko@gmail.com>
2023-03-17 13:16:28 +0600

    class Video has been changed

--- utils/video.py
+++ utils/video.py
@@ -3,13 +3,17 @@ from googleapiclient.discovery import build
 
 
 class Video():
+
     def __init__(self, video_id):
         self.video_id = video_id
-        youtube = self.build('youtube', 'v3', developerKey='AIzaSyC-4j13M0Xk0NnC4w4BVanF64fO5Lv0uUI')
-        self.video_response = youtube.videos().list(part='snippet,statistics', id=video_id).execute()
+        youtube = build('youtube', 'v3', developerKey='AIzaSyC-4j13M0Xk0NnC4w4BVanF64fO5Lv0uUI')
+        self.video_response = youtube.videos().list(part='snippet,statistics', id=self.video_id).execute()
         self.title = self.video_response['items'][0]['snippet']['title']
         self.views_count = self.video_response['items'][0]['statistics']['viewCount']
         self.likes_count = self.video_response['items'][0]['statistics']['likeCount']
+
+    def __repr__(self):
+        return f'{self.title}'
 
 class PLVideo(Video):
     def __init__(self, video_id, playlist_id):
