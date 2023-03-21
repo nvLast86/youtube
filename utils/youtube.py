6aaf52a41008f6b3c6aa996966327b189a3ca4b0
nvLast86 <n.lastenko@gmail.com>
2023-03-22 00:13:26 +0600

    little fixes of class YouTube

--- utils/youtube.py
+++ utils/youtube.py
@@ -1,17 +1,20 @@
 from abc import ABC
 
 
-class YouTube:
+class YouTube(ABC):
     """
     Абстрактный класс для будущего определения методов, которые будут переопределены
     в дочерних классах
     """
+
+    @abstractmethod
     def get_youtube_api_key(self):
         """
         Метод получения API_KEY для работы с Youtube
         """
         pass
 
+    @abstractmethod
     def get_youtube_object(self):
         """
         Метод получения объекта Youtube
