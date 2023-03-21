09a06e461a58221e6f23323eeabb985ea2ccc4c7
nvLast86 <n.lastenko@gmail.com>
2023-03-22 05:27:09 +0600

    classes Video and PLVideo have been updated due to abstract class YouTube appearance

--- utils/video.py
+++ utils/video.py
@@ -1,11 +1,17 @@
 from googleapiclient.discovery import build
-from additional_service import AdditionalService
+from youtube import YouTube
+import os
 
-class Video(AdditionalService):
+
+class Video(YouTube):
+    """
+    Класс Video для работы с видео Youtube
+    """
 
     def __init__(self, video_id):
         self.video_id = video_id
-        self.video_response = self.youtube.videos().list(part='snippet,statistics', id=self.video_id).execute()
+        self.video_response = self.get_youtube_object().videos().\
+            list(part='snippet,statistics', id=self.video_id).execute()
         self.title = self.video_response['items'][0]['snippet']['title']
         self.views_count = self.video_response['items'][0]['statistics']['viewCount']
         self.likes_count = self.video_response['items'][0]['statistics']['likeCount']
@@ -13,13 +19,28 @@ class Video(AdditionalService):
     def __repr__(self):
         return f'{self.title}'
 
+    def get_youtube_api_key(self):
+        """
+        Метод получения API_KEY для работы с Youtube
+        """
+        api_key = os.environ.get('YT_API_KEY')
+        return api_key
+
+    def get_youtube_object(self):
+        """
+        Метод получения объекта Youtube
+        """
+        youtube = build('youtube', 'v3', developerKey=self.get_youtube_api_key())
+        return youtube
+
 
 class PLVideo(Video):
+    """Класс PLVideo для работы с видео и плейлистами youtube"""
 
     def __init__(self, video_id, playlist_id):
         super().__init__(video_id)
         self.playlist_id = playlist_id
-        playlist = self.youtube.playlists().list(id=self.playlist_id, part='snippet').execute()
+        playlist = self.get_youtube_object().playlists().list(id=self.playlist_id, part='snippet').execute()
         self.playlist_name = playlist['items'][0]['snippet']['title']
 
     def __repr__(self):
