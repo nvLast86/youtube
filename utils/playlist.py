326e41c91794c9ab2dcec576b474ac507140e9d4
nvLast86 <n.lastenko@gmail.com>
2023-03-22 00:43:29 +0600

    show_best_video() has been added

--- utils/playlist.py
+++ utils/playlist.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta
 import isodate
 from googleapiclient.discovery import build
-from youtube import YouTube
+from utils.youtube import YouTube
 import os
 
 
@@ -9,13 +9,13 @@ class PlayList(YouTube):
 
     def __init__(self, playlist_id):
         self.playlist_id = playlist_id
-        self.playlist = self.youtube.playlists().list(id=playlist_id, part='snippet').execute()
+        self.playlist = self.get_youtube_object().playlists().list(id=playlist_id, part='snippet').execute()
         self.title = self.playlist['items'][0]['snippet']['title']
         self.url = 'https://www.youtube.com/playlist?list=' + playlist_id
-        self.playlist_videos = self.youtube.playlistItems().list(playlistId=playlist_id, part='contentDetails',
-                                                                 maxResults=50,).execute()
+        self.playlist_videos = self.get_youtube_object().playlistItems().\
+            list(playlistId=playlist_id, part='contentDetails', maxResults=50,).execute()
         self.video_ids = [video['contentDetails']['videoId'] for video in self.playlist_videos['items']]
-        self.video_response = self.youtube.videos().list(part='contentDetails,statistics', id=','
+        self.video_response = self.get_youtube_object().videos().list(part='contentDetails,statistics', id=','
                                                          .join(self.video_ids)).execute()
 
     def get_youtube_api_key(self):
@@ -35,3 +35,15 @@ class PlayList(YouTube):
             duration = isodate.parse_duration(iso_formation_duration)
             total_duration += duration
         return total_duration
+
+    def show_best_video(self):
+        """
+        Метод определения видео с наибольшим количеством лайков
+        """
+        max_likes = 0
+        for video in self.video_response['items']:
+            if int(video['statistics']['likeCount']) > max_likes:
+                max_likes = int(video['statistics']['likeCount'])
+        for video in self.video_response['items']:
+            if video['statistics']['likeCount'] == str(max_likes):
+                return f"https://www.youtube.com/watch?v={video['id']}"
